217b98290281b2854c8b0a08da4fb756cf3028b2
ozguraysenur <aysenur-97@hotmail.com>
2020-03-13 19:08:05 +0100

    exercises

---
+++ inheritance/AbstractClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //abstractinda interface inde objesi olusturulumaz.
+            DataBase sql = new SqlServer();
+            sql.add();
+            sql.Delete();
+
+        }
+
+        abstract class DataBase // abstract class interface ve virtual in karisimi
+        {                       //icinde bos metodda olabilir doluda
+            public void add()
+            {
+                Console.WriteLine("added by default");
+            }
+            public abstract void Delete();
+        }
+        class SqlServer : DataBase
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("deleted by sql");
+            }
+        }
+    }
+}
+
---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3]
+            {
+                new Customer{name ="ayse"},
+                new student{name="Bilal"},
+                new Person{name="reyhan"}
+            };
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.talk());
+                Console.WriteLine(person.name);
+            }
+
+
+        }
+    }
+    class Person
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public virtual string talk() { return "Helloo"; }  //virtual method metodu kendine gore degisterebilirsin
+    }
+    class student : Person //iki tane class i inherit edemiyorsun (ilk once inheritance sonra interfaceler )
+    {                      //ama interface te olur 
+        public string city { get; set; }
+
+    }
+    class Customer : Person
+    {
+        public int department { get; set; }
+        public override string talk()  //overriding
+        {
+            // return base.talk();
+            return "hiii";
+        }
+    }
+}
